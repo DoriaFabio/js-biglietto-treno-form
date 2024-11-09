fa1ca24b33c3bac5a5d495dda7c97ddec64598c4
DoriaFabio <doriafabio2001@gmail.com>
2024-11-09 20:46:49 +0100

    do code js for take price of train ticket and transport to html

--- script.js
+++ script.js
@@ -6,27 +6,47 @@ console.clear();
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 // - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-// const km = prompt("Inserisci il numero di km");
-// console.log("L'utente vuole percorrere " + km + " km");
-// const age = prompt("Inserisci l'età");
-// console.log("L'utente ha " + age + " anni");
-// let prezzoTotale = 0.21*km;
-// let prezzoScontato;
-// let messaggio = " ";
-// if (age<=18) {
-//     prezzoScontato = prezzoTotale - (prezzoTotale * 0.2); 
-//     messaggio = ("Il prezzo scontato per l'utente minorenne è ");
-//     console.log (messaggio + prezzoScontato.toFixed(2) + " euro");
-// } else if (age>=65) {
-//     prezzoScontato = prezzoTotale - (prezzoTotale * 0.4); 
-//     messaggio = ("Il prezzo scontato per l'utente over 65 è ");
-//     console.log (messaggio + prezzoScontato.toFixed(2) + " euro");
-// } else {
-//     console.log("Il prezzo totale è " + prezzoTotale + " euro");
-// }
 
-const myform = document.getElementById("login");
-console.log(myform);
-myform.addEventListener("submit", function(event) {
+// Recuperiamo gli elementi di interesse dal FORM
+const nameField = document.getElementById("nome-field");
+const surnameField = document.getElementById("cognome-field");
+const kmField = document.getElementById("km-field");
+const etàField = document.getElementById("età-field");
+const myform = document.querySelector("form");
+// Recuperiamo gli elementi di interesse dal biglietto da stampare
+const myname = document.getElementById("nome");
+const mysurname = document.getElementById("cognome");
+const km = document.getElementById("km");
+const età = document.getElementById("età");
+const ticket = document.getElementById("ticket");
+const prezzoScontato = document.getElementById("prezzoScontato");
+
+console.log(kmField);
+// All'invio del form, aggiorniamo i valori del biglietto
+myform.addEventListener("submit", function (event) {
     event.preventDefault();
+    ticket.classList.remove("d-none");
+    myname.innerHTML = nameField.value;
+    mysurname.innerHTML = surnameField.value;
+    km.innerHTML = kmField.value;
+    età.innerHTML = etàField.value;
+    // Calcolo prezzo
+    let prezzoTotale = 0.21 * km.innerHTML;
+    console.log(prezzoTotale);
+    if (età.innerHTML < "18") {
+        const priceMin = prezzoTotale - (prezzoTotale * 0.2);
+        prezzoScontato.innerHTML = "Sei minorenne quindi il prezzo è " + priceMin.toFixed(2) + "€";
+    } else if (età.innerHTML >= "18" && età.innerHTML < "65") {
+        prezzoScontato.innerHTML = "Non c'è nessuno sconto, il prezzo base è " + prezzoTotale;
+
+    } else if (età.innerHTML >= "65") {
+        const priceSen = prezzoTotale - (prezzoTotale * 0.4);
+        prezzoScontato.innerHTML = "Sei OVER65 quindi il prezzo è " + priceSen.toFixed(2) + "€";
+    }
 });
+
+// const myform = document.getElementById("login");
+// console.log(myform);
+// myform.addEventListener("submit", function(event) {
+//     event.preventDefault();
+// });
